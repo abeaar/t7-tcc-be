63a3051c020795d567ed11f115d89dd919063457
abeaar <abraarjh@gmail.com>
2025-05-20 20:47:48 +0700

    update origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 dotenv.config();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: 'http://localhost:5000' }));
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 app.use(UserRoute);
